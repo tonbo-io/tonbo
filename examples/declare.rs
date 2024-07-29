be040a08ae2e4b1ae4f8bcf3e9e2d115b3bfcaac
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-07-29 17:15:09 +0800

    feat: more test type in examples

--- examples/declare.rs
+++ examples/declare.rs
@@ -5,6 +5,15 @@ use morseldb::morsel_record;
 pub struct Post {
     #[primary_key]
     name: String,
+    vu8: u8,
+    vu16: u16,
+    vu32: u32,
+    vu64: u64,
+    vi8: i8,
+    vi16: i16,
+    vi32: i32,
+    vi64: i64,
+    vbool: bool,
 }
 
 fn main() {
