6decf9557bc13ea52902278de747cf61f324ed4e
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-08-01 23:19:25 +0800

    feat: update syn to 2.x

--- src/tonbo_marco/src/schema_model.rs
+++ src/tonbo_marco/src/schema_model.rs
@@ -5,7 +5,7 @@ pub(crate) struct ModelAttributes;
 impl ModelAttributes {
     pub(crate) fn parse_field(field: &Field) -> Result<bool> {
         for attr in &field.attrs {
-            if attr.path.is_ident("primary_key") {
+            if attr.path().is_ident("primary_key") {
                 return Ok(true);
             }
         }
