a3344c4bf6690621b355476f6db0ce11bb5f0c9b
Kould <kould2333@gmail.com>
2024-08-02 14:02:29 +0800

    feat: impl `Key` for num

--- src/tonbo_marco/src/keys.rs
+++ src/tonbo_marco/src/keys.rs
@@ -4,6 +4,7 @@ use syn::Type;
 pub(crate) struct PrimaryKey {
     pub(crate) name: Ident,
     pub(crate) base_ty: Type,
+    pub(crate) fn_key: TokenStream,
     pub(crate) builder_append_value: TokenStream,
     pub(crate) index: usize,
 }
