b66d947a3a279ea2724f8e8959a743a838386f28
Kould <kould2333@gmail.com>
2024-08-20 12:02:50 +0800

    fix: directly parse tbl

--- benchmarks/read_bench.rs
+++ benchmarks/read_bench.rs
@@ -11,8 +11,8 @@ use std::{
 use futures_util::{future::join_all, StreamExt};
 
 use crate::common::{
-    read_csv, BenchDatabase, BenchReadTransaction, BenchReader, RedbBenchDatabase,
-    RocksdbBenchDatabase, SledBenchDatabase, TonboBenchDataBase, ITERATIONS, NUM_SCAN, READ_TIMES,
+    read_tbl, BenchDatabase, BenchReadTransaction, BenchReader, RedbBenchDatabase,
+    RocksdbBenchDatabase, SledBenchDatabase, TonboBenchDataBase, ITERATIONS, READ_TIMES,
 };
 
 async fn benchmark<T: BenchDatabase + Send + Sync>(
@@ -35,24 +35,15 @@ async fn benchmark<T: BenchDatabase + Send + Sync>(
         for _ in 0..ITERATIONS {
             let start = Instant::now();
             let reader = txn.get_reader();
-            let mut value_sum = 0;
             for _ in 0..READ_TIMES {
                 let mut iter = Box::pin(reader.range_from((Bound::Unbounded, Bound::Unbounded)));
-                for _ in 0..NUM_SCAN {
-                    if let Some(_record) = iter.next().await {
-                        value_sum += 1;
-                    } else {
-                        break;
-                    }
-                }
+                while let Some(_result) = iter.next().await {}
             }
-            assert!(value_sum > 0);
             let end = Instant::now();
             let duration = end - start;
             println!(
-                "{}: Random range read {} elements in {}ms",
+                "{}: Random range read in {}ms",
                 T::db_type_name(),
-                READ_TIMES * NUM_SCAN,
                 duration.as_millis()
             );
             results.push(("random range reads".to_string(), duration));
@@ -61,25 +52,16 @@ async fn benchmark<T: BenchDatabase + Send + Sync>(
         for _ in 0..ITERATIONS {
             let start = Instant::now();
             let reader = txn.get_reader();
-            let mut value_sum = 0;
             for _ in 0..READ_TIMES {
                 let mut iter =
                     Box::pin(reader.projection_range_from((Bound::Unbounded, Bound::Unbounded)));
-                for _ in 0..NUM_SCAN {
-                    if let Some(_projection_field) = iter.next().await {
-                        value_sum += 1;
-                    } else {
-                        break;
-                    }
-                }
+                while let Some(_result) = iter.next().await {}
             }
-            assert!(value_sum > 0);
             let end = Instant::now();
             let duration = end - start;
             println!(
-                "{}: Random range projection read {} elements in {}ms",
+                "{}: Random range projection read in {}ms",
                 T::db_type_name(),
-                READ_TIMES * NUM_SCAN,
                 duration.as_millis()
             );
             results.push(("random range projection reads".to_string(), duration));
@@ -95,20 +77,12 @@ async fn benchmark<T: BenchDatabase + Send + Sync>(
 
             async move {
                 let txn = db2.read_transaction().await;
-                let mut value_sum = 0;
                 let reader = txn.get_reader();
                 for _ in 0..(READ_TIMES / num_threads) {
                     let mut iter = Box::pin(
                         reader.projection_range_from((Bound::Unbounded, Bound::Unbounded)),
                     );
-                    for _ in 0..NUM_SCAN {
-                        if let Some(_projection_field) = iter.next().await {
-                            value_sum += 1;
-                        } else {
-                            break;
-                        }
-                    }
-                    assert!(value_sum > 0);
+                    while let Some(_result) = iter.next().await {}
                 }
             }
         });
@@ -117,10 +91,9 @@ async fn benchmark<T: BenchDatabase + Send + Sync>(
         let end = Instant::now();
         let duration = end - start;
         println!(
-            "{}: Random range projection reads ({} threads) {} elements in {}ms",
+            "{}: Random range projection reads ({} threads) in {}ms",
             T::db_type_name(),
             num_threads,
-            READ_TIMES * NUM_SCAN,
             duration.as_millis()
         );
         results.push((
@@ -139,24 +112,26 @@ async fn main() {
     {
         use crate::common::{BenchInserter, BenchWriteTransaction};
 
-        let csv_path = current_dir().unwrap().join("customer.csv");
+        let tbl_path = current_dir().unwrap().join("./benchmark/customer.tbl");
 
-        async fn load<T: BenchDatabase>(csv_path: impl AsRef<Path>, path: impl AsRef<Path>) {
+        async fn load<T: BenchDatabase>(tbl_path: impl AsRef<Path>, path: impl AsRef<Path>) {
+            println!("{}: start loading", T::db_type_name());
             let database = T::build(path).await;
             let mut tx = database.write_transaction().await;
             let mut inserter = tx.get_inserter();
 
-            for customer in read_csv(csv_path).unwrap() {
+            for customer in read_tbl(tbl_path) {
                 inserter.insert(customer).unwrap();
             }
             drop(inserter);
             tx.commit().await.unwrap();
+            println!("{}: loading completed", T::db_type_name());
         }
 
-        load::<TonboBenchDataBase>(&csv_path, data_dir.join("tonbo")).await;
-        load::<RocksdbBenchDatabase>(&csv_path, data_dir.join("rocksdb")).await;
-        load::<RedbBenchDatabase>(&csv_path, data_dir.join("redb")).await;
-        load::<SledBenchDatabase>(&csv_path, data_dir.join("sled")).await;
+        load::<TonboBenchDataBase>(&tbl_path, data_dir.join("tonbo")).await;
+        load::<RocksdbBenchDatabase>(&tbl_path, data_dir.join("rocksdb")).await;
+        load::<RedbBenchDatabase>(&tbl_path, data_dir.join("redb")).await;
+        load::<SledBenchDatabase>(&tbl_path, data_dir.join("sled")).await;
     }
 
     let tonbo_latency_results = { benchmark::<TonboBenchDataBase>(data_dir.join("tonbo")).await };
