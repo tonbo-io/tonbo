6d31bbaa0ccb5f41af195a00b641d92a991c06d7
crwen <1543720935@qq.com>
2024-10-09 18:39:31 +0800

    remove immutable properties in DbOption

--- bindings/python/src/options.rs
+++ bindings/python/src/options.rs
@@ -3,35 +3,23 @@ use pyo3::{pyclass, pymethods};
 use tonbo::record::DynRecord;
 
 /// configure the operating parameters of each component in the `DB`
-#[pyclass]
+#[pyclass(get_all, set_all)]
 #[derive(Debug, Clone)]
-#[allow(unused)]
 pub struct DbOption {
     /// cached message size in parquet cleaner
-    #[pyo3(get, set)]
     clean_channel_buffer: usize,
     /// len threshold of `immutables` when minor compaction is triggered
-    #[pyo3(get, set)]
     immutable_chunk_num: usize,
-    immutable_chunk_max_num: usize,
     /// magnification that triggers major compaction between different levels
-    #[pyo3(get, set)]
     level_sst_magnification: usize,
-    #[pyo3(get, set)]
     major_default_oldest_table_num: usize,
-    major_l_selection_table_max_num: usize,
     /// threshold for the number of `parquet` when major compaction is triggered
-    #[pyo3(get, set)]
     major_threshold_with_sst_size: usize,
     /// Maximum size of each parquet
-    #[pyo3(get, set)]
     max_sst_file_size: usize,
-    #[pyo3(get, set)]
     version_log_snapshot_threshold: u32,
-    #[pyo3(get, set)]
     use_wal: bool,
     /// build the `DB` storage directory based on the passed path
-    #[pyo3(get, set)]
     path: String,
 }
 
@@ -40,16 +28,14 @@ impl DbOption {
     #[new]
     pub(crate) fn new(path: String) -> Self {
         Self {
-            clean_channel_buffer: 0,
-            immutable_chunk_num: 0,
-            immutable_chunk_max_num: 0,
-            level_sst_magnification: 0,
-            major_default_oldest_table_num: 0,
-            major_l_selection_table_max_num: 0,
-            major_threshold_with_sst_size: 0,
-            max_sst_file_size: 0,
-            version_log_snapshot_threshold: 0,
-            use_wal: false,
+            clean_channel_buffer: 10,
+            immutable_chunk_num: 3,
+            level_sst_magnification: 10,
+            major_default_oldest_table_num: 3,
+            major_threshold_with_sst_size: 4,
+            max_sst_file_size: 256 * 1024 * 1024,
+            version_log_snapshot_threshold: 200,
+            use_wal: true,
             path,
         }
     }
@@ -62,10 +48,21 @@ impl DbOption {
         primary_key_name: String,
     ) -> tonbo::DbOption<DynRecord> {
         std::fs::create_dir_all(self.path.clone()).unwrap();
-        tonbo::DbOption::with_path(
+        let mut opt = tonbo::DbOption::with_path(
             Path::from_filesystem_path(self.path).unwrap(),
             primary_key_name,
             primary_key_index,
         )
+        .clean_channel_buffer(self.clean_channel_buffer)
+        .immutable_chunk_num(self.immutable_chunk_num)
+        .level_sst_magnification(self.level_sst_magnification)
+        .major_default_oldest_table_num(self.major_default_oldest_table_num)
+        .major_threshold_with_sst_size(self.major_threshold_with_sst_size)
+        .max_sst_file_size(self.max_sst_file_size)
+        .version_log_snapshot_threshold(self.version_log_snapshot_threshold);
+        if !self.use_wal {
+            opt = opt.disable_wal()
+        }
+        opt
     }
 }
