e163ebf41775244aaa5edb497d9a6c307f7b9ccd
crwen <1543720935@qq.com>
2025-06-16 10:27:07 +0800

    feat: add date32/date64/time32/time64 type

--- src/record/key/timestamp.rs
+++ src/record/key/timestamp.rs
@@ -11,7 +11,9 @@ use arrow::array::{
 use chrono::{DateTime, NaiveDateTime};
 use fusio_log::{Decode, Encode};
 
-use super::{Key, KeyRef};
+use crate::record::{MICROSECONDS, MILLISECONDS, NANOSECONDS, SECONDS_IN_DAY};
+
+use super::{Date32, Date64, Key, KeyRef, Time32, Time64};
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TimeUnit {
@@ -22,7 +24,7 @@ pub enum TimeUnit {
 }
 
 impl TimeUnit {
-    fn factor(&self) -> i64 {
+    pub(crate) fn factor(&self) -> i64 {
         match self {
             TimeUnit::Second => 1_000_000_000,
             TimeUnit::Millisecond => 1_000_000,
@@ -206,6 +208,15 @@ impl Timestamp {
         }
     }
 
+    pub fn timestamp(&self) -> i64 {
+        match self.unit {
+            TimeUnit::Second => self.ts,
+            TimeUnit::Millisecond => self.ts.saturating_mul(1000),
+            TimeUnit::Microsecond => self.ts.saturating_mul(1_000_000),
+            TimeUnit::Nanosecond => self.ts.saturating_mul(1_000_000_000),
+        }
+    }
+
     pub fn timestamp_millis(&self) -> i64 {
         match self.unit {
             TimeUnit::Second => self.ts.saturating_div(1_000),
@@ -238,25 +249,23 @@ impl Timestamp {
         let utc = naive.and_utc();
         match unit {
             TimeUnit::Second => {
-                let seconds = utc.timestamp();
-                seconds
-                    .checked_add(utc.timestamp_subsec_nanos() as i64)
-                    .map(|ts| Timestamp { ts, unit })
+                let ts = utc.timestamp();
+                Some(Timestamp { ts, unit })
             }
             TimeUnit::Millisecond => {
-                let millis = utc.timestamp().checked_mul(1_000)?;
+                let millis = utc.timestamp().checked_mul(MILLISECONDS)?;
                 millis
                     .checked_add(utc.timestamp_subsec_millis() as i64)
                     .map(|ts| Timestamp { ts, unit })
             }
             TimeUnit::Microsecond => {
-                let micros = utc.timestamp().checked_mul(1_000_000)?;
+                let micros = utc.timestamp().checked_mul(MICROSECONDS)?;
                 micros
                     .checked_add(utc.timestamp_subsec_micros() as i64)
                     .map(|ts| Timestamp { ts, unit })
             }
             TimeUnit::Nanosecond => {
-                let nanos = utc.timestamp().checked_mul(1_000_000_000)?;
+                let nanos = utc.timestamp().checked_mul(NANOSECONDS)?;
                 nanos
                     .checked_add(utc.timestamp_subsec_nanos() as i64)
                     .map(|ts| Timestamp { ts, unit })
@@ -273,12 +282,59 @@ impl Timestamp {
             TimeUnit::Nanosecond => Some(DateTime::from_timestamp_nanos(self.ts).naive_utc()),
         }
     }
+
+    /// convert [`Timestamp`] to [`Time32`] in second
+    pub fn to_time32_second(&self) -> Option<Time32> {
+        let datetime = self.to_naive_date_time()?;
+        Some(Time32::from_naive_time_second(datetime.time()))
+    }
+
+    /// convert [`Timestamp`] to [`Time32`] in millisecond
+    pub fn to_time32_millisecond(&self) -> Option<Time32> {
+        let datetime = self.to_naive_date_time()?;
+        Some(Time32::from_naive_time_millisecond(datetime.time()))
+    }
+
+    /// convert [`Timestamp`] to [`Time64`] in microsecond
+    pub fn to_time64_microsecond(&self) -> Option<Time64> {
+        let datetime = self.to_naive_date_time()?;
+        Some(Time64::from_naive_time_microsecond(datetime.time()))
+    }
+
+    /// convert [`Timestamp`] to [`Time64`] in nanosecond
+    pub fn to_time64_nanosecond(&self) -> Option<Time64> {
+        let datetime = self.to_naive_date_time()?;
+        Some(Time64::from_naive_time_nanosecond(datetime.time()))
+    }
+
+    /// convert [`Timestamp`] to [`Date32`] in days
+    pub fn to_date32(&self) -> Date32 {
+        let days = match self.unit {
+            TimeUnit::Second => self.ts / SECONDS_IN_DAY,
+            TimeUnit::Millisecond => self.ts / SECONDS_IN_DAY / MILLISECONDS,
+            TimeUnit::Microsecond => self.ts / SECONDS_IN_DAY / MICROSECONDS,
+            TimeUnit::Nanosecond => self.ts / SECONDS_IN_DAY / NANOSECONDS,
+        };
+        Date32::new(days as i32)
+    }
+
+    /// convert [`Timestamp`] to [`Date64`] in millisecond
+    pub fn to_date64(&self) -> Date64 {
+        let millis = match self.unit {
+            TimeUnit::Second => self.ts * MILLISECONDS,
+            TimeUnit::Millisecond => self.ts,
+            TimeUnit::Microsecond => self.ts / MILLISECONDS,
+            TimeUnit::Nanosecond => self.ts / MICROSECONDS,
+        };
+        Date64::new(millis)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::io::{Cursor, SeekFrom};
 
+    use chrono::Utc;
     use fusio_log::{Decode, Encode};
     use tokio::io::AsyncSeekExt;
 
@@ -295,5 +351,67 @@ mod tests {
         let ts2 = Timestamp::decode(&mut buf).await.unwrap();
 
         assert_eq!(ts, ts2);
+    }
+
+    #[test]
+    fn test_timestamp_to_naive_date_time() {
+        let datetime = Utc::now();
+        let expected = datetime.naive_utc();
+
+        // test nanoseconds
+        let nanoseconds = datetime.timestamp_nanos_opt().unwrap();
+        let ts = Timestamp::new_nanos(nanoseconds);
+        assert_eq!(ts.to_naive_date_time(), Some(expected));
+
+        // test microseconds
+        let microseconds = datetime.timestamp_micros();
+        let ts = Timestamp::new_micros(microseconds);
+        assert_eq!(ts.to_naive_date_time(), Some(expected));
+
+        // test milliseconds
+        let milliseconds = datetime.timestamp_millis();
+        let ts = Timestamp::new_millis(milliseconds);
+        assert_eq!(
+            ts.to_naive_date_time(),
+            Some(
+                DateTime::from_timestamp_millis(milliseconds)
+                    .unwrap()
+                    .naive_utc()
+            ),
+        );
+
+        // test seconds
+        let seconds = datetime.timestamp();
+        let ts = Timestamp::new_seconds(seconds);
+        assert_eq!(
+            ts.to_naive_date_time(),
+            Some(DateTime::from_timestamp(seconds, 0).unwrap().naive_utc()),
+        );
+    }
+
+    #[test]
+    fn test_timestamp_from_naive_date_time() {
+        let datetime = Utc::now();
+        let utc = datetime.naive_utc();
+
+        {
+            let ts = Timestamp::from_naive_date_time(utc, TimeUnit::Nanosecond).unwrap();
+            assert_eq!(
+                ts.timestamp_nanos(),
+                datetime.timestamp_nanos_opt().unwrap()
+            );
+        }
+        {
+            let ts = Timestamp::from_naive_date_time(utc, TimeUnit::Microsecond).unwrap();
+            assert_eq!(ts.timestamp_micros(), datetime.timestamp_micros());
+        }
+        {
+            let ts = Timestamp::from_naive_date_time(utc, TimeUnit::Millisecond).unwrap();
+            assert_eq!(ts.timestamp_millis(), datetime.timestamp_millis());
+        }
+        {
+            let ts = Timestamp::from_naive_date_time(utc, TimeUnit::Second).unwrap();
+            assert_eq!(ts.timestamp(), datetime.timestamp());
+        }
     }
 }
