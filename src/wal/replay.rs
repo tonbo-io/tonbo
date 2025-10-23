b6690e8264a10f0d2b1987bbaf0d6c297aaae841
Tzu <tzu@tonbo.io>
2025-10-23 17:56:30 +0800

    chore: make clippy happy

--- src/wal/replay.rs
+++ src/wal/replay.rs
@@ -3,8 +3,8 @@
 use std::{fs, io, path::PathBuf};
 
 use crate::wal::{
-    frame::{decode_frame, FrameHeader, WalEvent, FRAME_HEADER_SIZE},
     WalConfig, WalError, WalResult,
+    frame::{FRAME_HEADER_SIZE, FrameHeader, WalEvent, decode_frame},
 };
 
 /// Scans WAL segments on disk and yields decoded events.
@@ -82,14 +82,14 @@ mod tests {
 
     use arrow_array::{Int32Array, RecordBatch, StringArray};
     use arrow_schema::{DataType, Field, Schema};
-    use crate::wal::{batch_with_tombstones, strip_tombstone_column};
 
     use super::*;
     use crate::{
         mvcc::Timestamp,
         wal::{
-            WalPayload,
+            WalPayload, batch_with_tombstones,
             frame::{INITIAL_FRAME_SEQ, encode_payload},
+            strip_tombstone_column,
         },
     };
 
